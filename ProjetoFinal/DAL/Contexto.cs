61043eb0bcf4774346400fe176d67535bdcced0a
Guilherme Painko Scalcon <guilherme.Painko@outlook.com>
2025-07-04 10:32:20 -0300

    atualizacao final

---
+++ ProjetoFinal/DAL/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Models;
+
+namespace MVC.DAL
+{
+    public class Contexto :DbContext
+    {
+        public DbSet<Projeto> Projetos { get; set; }
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tarefa>()
+                .HasOne(t => t.Projeto)
+                .WithMany(p => p.Tarefas)
+                .HasForeignKey(t => t.ProjetoId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
